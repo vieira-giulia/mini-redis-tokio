76f0123485cccc4da76540638799c8c927bf9606
Giulia Vieira <giuliamsgvieira@gmail.com>
2022-02-09 21:54:47 -0300

    adding frames, and reading frames

---
+++ my-redis/src/frames.rs
@@ -0,0 +1,81 @@
+use tokio::net::TcpStream;
+use mini_redis::{Frame, Result};
+use bytes::Bytes;
+use tokio::io::AsyncReadExt;
+use bytes::Buf;
+
+enum Frame {
+    Simple(String),
+    Error(String),
+    Integer(u64),
+    Bulk(Bytes),
+    Null,
+    Array(Vec<Frame>),
+}
+
+enum HttpFrame {
+    RequestHead {
+        method: Method,
+        uri: Uri,
+        version: Version,
+        headers: HeaderMap,
+    },
+    ResponseHead {
+        status: StatusCode,
+        version: Version,
+        headers: HeaderMap,
+    },
+    BodyChunk {
+        chunk: Bytes,
+    },
+}
+
+struct Connection {
+    stream: TcpStream,
+    buffer: BytesMut,
+    cursor: usize,
+}
+
+impl Connection {
+    pub fn new(stream: TcpStream) -> Connection {
+        Connection {
+            stream,
+            // Allocate the buffer with 4kb of capacity.
+            buffer: vec![0; 4096],
+            cursor: 0,
+        }
+    }
+}
+
+pub async fn read_frame(&mut self)
+    -> Result<Option<Frame>>
+{
+    loop {
+        if let Some(frame) = self.parse_frame()? {
+            return Ok(Some(frame));
+        }
+
+        // Ensure the buffer has capacity
+        if self.buffer.len() == self.cursor {
+            // Grow the buffer
+            self.buffer.resize(self.cursor * 2, 0);
+        }
+
+        // Read into the buffer, tracking the number
+        // of bytes read
+        let n = self.stream.read(
+            &mut self.buffer[self.cursor..]).await?;
+
+        if 0 == n {
+            if self.cursor == 0 {
+                return Ok(None);
+            } else {
+                return Err("connection reset by peer".into());
+            }
+        } else {
+            // Update our cursor
+            self.cursor += n;
+        }
+    }
+}
+
