ad7e815a94bda0ba265ab16971429cd131d11cec
Giulia Vieira <giuliamsgvieira@gmail.com>
2022-02-09 21:47:28 -0300

    separate write and read

--- my-redis/src/bin/echo-server-copy.rs
+++ my-redis/src/bin/echo-server-copy.rs
@@ -1,15 +1,32 @@
-use tokio::io;
-use tokio::net::TcpListener;
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6142").await?;
+    let socket = TcpStream::connect("127.0.0.1:6142").await?;
+    let (mut rd, mut wr) = io::split(socket);
+
+    // Write data in the background
+    tokio::spawn(async move {
+        wr.write_all(b"hello\r\n").await?;
+        wr.write_all(b"world\r\n").await?;
+
+        // Sometimes, the rust type inferencer needs
+        // a little help
+        Ok::<_, io::Error>(())
+    });
+
+    let mut buf = vec![0; 128];
 
     loop {
-        let (mut socket, _) = listener.accept().await?;
+        let n = rd.read(&mut buf).await?;
 
-        tokio::spawn(async move {
-            // Copy data here
-        });
+        if n == 0 {
+            break;
+        }
+
+        println!("GOT {:?}", &buf[..n]);
     }
+
+    Ok(())
 }
