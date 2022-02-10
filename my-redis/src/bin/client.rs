4e7de007e7ea9763ae77772aeb74e6eefeed53a3
Giulia Vieira <giuliamsgvieira@gmail.com>
2022-02-09 21:37:55 -0300

    adding responder

--- my-redis/src/bin/client.rs
+++ my-redis/src/bin/client.rs
@@ -1,5 +1,26 @@
 use tokio::sync::mpsc;
 use mini_redis::client;
+use tokio::sync::oneshot;
+use bytes::Bytes;
+
+
+/// Multiple different commands are multiplexed over a single channel.
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+        resp: Responder<Option<Bytes>>,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+        resp: Responder<()>,
+    },
+}
+
+/// Provided by the requester and used by the manager task to send
+/// the command response back to the requester.
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 // The `move` keyword is used to **move** ownership of `rx` into the task.
 let manager = tokio::spawn(async move {
