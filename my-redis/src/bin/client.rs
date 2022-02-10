ffadd11a99b8e8e10ac639a4497c57d87b0723d5
Giulia Vieira <giuliamsgvieira@gmail.com>
2022-02-09 21:36:54 -0300

    adding tasks management

--- my-redis/src/bin/client.rs
+++ my-redis/src/bin/client.rs
@@ -1,9 +1,66 @@
 use tokio::sync::mpsc;
+use mini_redis::client;
+
+// The `move` keyword is used to **move** ownership of `rx` into the task.
+let manager = tokio::spawn(async move {
+    // Establish a connection to the server
+    let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+    // Start receiving messages
+    while let Some(cmd) = rx.recv().await {
+        use Command::*;
+
+        match cmd {
+            Get { key } => {
+                client.get(&key).await;
+            }
+            Set { key, val } => {
+                client.set(&key, val).await;
+            }
+        }
+    }
+});
+
+// The `Sender` handles are moved into the tasks. As there are two
+// tasks, we need a second `Sender`.
+let tx2 = tx.clone();
+
+// Spawn two tasks, one gets a key, the other sets a key
+let t1 = tokio::spawn(async move {
+    let cmd = Command::Get {
+        key: "hello".to_string(),
+    };
+
+    tx.send(cmd).await.unwrap();
+});
+
+let t2 = tokio::spawn(async move {
+    let cmd = Command::Set {
+        key: "foo".to_string(),
+        val: "bar".into(),
+    };
+
+    tx2.send(cmd).await.unwrap();
+});
 
 #[tokio::main]
 async fn main() {
-    // Create a new channel with a capacity of at most 32.
     let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
 
-    // ... Rest comes here
+    tokio::spawn(async move {
+        tx.send("sending from first handle").await;
+    });
+
+    tokio::spawn(async move {
+        tx2.send("sending from second handle").await;
+    });
+
+    while let Some(message) = rx.recv().await {
+        println!("GOT = {}", message);
+    }
+
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
 }
