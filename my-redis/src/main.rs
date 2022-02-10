f81d07d2ae9f5d31d527c627a7dd2e932ee50462
Giulia Vieira <giuliamsgvieira@gmail.com>
2022-02-09 21:29:35 -0300

    best practices for holding mutex guard. Separate structures so they dont deadlock. Mostly always better to refactor code than use more complicated structures

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -7,6 +7,22 @@ use std::sync::{Arc, Mutex};
 
 type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
+struct CanIncrement {
+    mutex: Mutex<i32>,
+}
+impl CanIncrement {
+    // This function is not marked async.
+    fn increment(&self) {
+        let mut lock = self.mutex.lock().unwrap();
+        *lock += 1;
+    }
+}
+
+async fn increment_and_do_stuff(can_incr: &CanIncrement) {
+    can_incr.increment();
+    //do_something_async().await;
+}
+
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
