308a17e378a11cf9ff46bd7dad1f7ea2956488b4
Giulia Vieira <giuliamsgvieira@gmail.com>
2022-02-09 22:10:41 -0300

    fix delay-future

--- my-redis/src/delay-future.rs
+++ my-redis/src/delay-future.rs
@@ -1,28 +1,40 @@
 use std::future::Future;
 use std::pin::Pin;
-use std::task::{Context, Poll};
-use std::time::{Duration, Instant};
+use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll, Waker};
 use std::thread;
+use std::time::{Duration, Instant};
 
 struct Delay {
     when: Instant,
+    // This Some when we have spawned a thread, and None otherwise.
+    waker: Option<Arc<Mutex<Waker>>>,
 }
 
 impl Future for Delay {
-    type Output = &'static str;
+    type Output = ();
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>)
-        -> Poll<&'static str>
-    {
-        if Instant::now() >= self.when {
-            println!("Hello world");
-            Poll::Ready("done")
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+        // First, if this is the first time the future is called, spawn the
+        // timer thread. If the timer thread is already running, ensure the
+        // stored `Waker` matches the current task's waker.
+        if let Some(waker) = &self.waker {
+            let mut waker = waker.lock().unwrap();
+
+            // Check if the stored waker matches the current task's waker.
+            // This is necessary as the `Delay` future instance may move to
+            // a different task between calls to `poll`. If this happens, the
+            // waker contained by the given `Context` will differ and we
+            // must update our stored waker to reflect this change.
+            if !waker.will_wake(cx.waker()) {
+                *waker = cx.waker().clone();
+            }
         } else {
-            // Get a handle to the waker for the current task
-            let waker = cx.waker().clone();
             let when = self.when;
+            let waker = Arc::new(Mutex::new(cx.waker().clone()));
+            self.waker = Some(waker.clone());
 
-            // Spawn a timer thread.
+            // This is the first time `poll` is called, spawn the timer thread.
             thread::spawn(move || {
                 let now = Instant::now();
 
@@ -30,9 +42,33 @@ impl Future for Delay {
                     thread::sleep(when - now);
                 }
 
-                waker.wake();
+                // The duration has elapsed. Notify the caller by invoking
+                // the waker.
+                let waker = waker.lock().unwrap();
+                waker.wake_by_ref();
             });
+        }
 
+        // Once the waker is stored and the timer thread is started, it is
+        // time to check if the delay has completed. This is done by
+        // checking the current instant. If the duration has elapsed, then
+        // the future has completed and `Poll::Ready` is returned.
+        if Instant::now() >= self.when {
+            Poll::Ready(())
+        } else {
+            // The duration has not elapsed, the future has not completed so
+            // return `Poll::Pending`.
+            //
+            // The `Future` trait contract requires that when `Pending` is
+            // returned, the future ensures that the given waker is signalled
+            // once the future should be polled again. In our case, by
+            // returning `Pending` here, we are promising that we will
+            // invoke the given waker included in the `Context` argument
+            // once the requested duration has elapsed. We ensure this by
+            // spawning the timer thread above.
+            //
+            // If we forget to invoke the waker, the task will hang
+            // indefinitely.
             Poll::Pending
         }
     }
