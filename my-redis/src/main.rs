b34b435919c05e2c332de8e8098e507cf6ecb2cd
Giulia Vieira <giuliamsgvieira@gmail.com>
2022-02-09 21:14:41 -0300

    async hello world

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -1,17 +1,15 @@
-use mini_redis::{client, Result};
+async fn say_world() {
+    println!("world");
+}
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    // Open a connection to the mini-redis address.
-    let mut client = client::connect("127.0.0.1:6379").await?;
+async fn main() {
+    // Calling `say_world()` does not execute the body of `say_world()`.
+    let op = say_world();
 
-    // Set the key "hello" with value "world"
-    client.set("hello", "world".into()).await?;
+    // This println! comes first
+    println!("hello");
 
-    // Get key "hello"
-    let result = client.get("hello").await?;
-
-    println!("got value from the server; result={:?}", result);
-
-    Ok(())
+    // Calling `.await` on `op` starts executing `say_world`.
+    op.await;
 }
